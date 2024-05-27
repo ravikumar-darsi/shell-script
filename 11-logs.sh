2ee0d94bea8936e7687aa246c1be18bdb0cb4993
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-05-27 12:35:35 +0530

    changes in 11th script

--- 11-logs.sh
+++ 11-logs.sh
@@ -3,6 +3,9 @@
 ID=$(id -u)
 
 echo "script name: $0"
+TIMESTAMP=$(date +%F-%H-%M-%S)
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -22,10 +25,10 @@ else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
-yum install mysql -y
+yum install mysql -y &>> $LOGFILE
 
 VALIDATE $? "Installing MySQL"
 
-yum install git -y
+yum install git -y &>> $LOGFILE
 
 VALIDATE $? "Installing GIT"
