95f45ae932222955fdd685271064bd8a9aef2717
ravikumar-darsi <ravikumardarsi90@gmail.com>
2024-05-29 14:44:01 +0530

    added script for 17th

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -16,6 +16,6 @@ done <<< $DISK_USAGE
 
 echo -e "Message: $message"
 
-#echo "$message" | mail -s "High Disk Usage" info@joindevops.com
+echo "$message" | mail -s "High Disk Usage" ravikumardarsi390@gmail.com
 
-sh mail.sh "DevOps Team" "High Disk Usage" "$message" "info@joindevops.com" "ALERT High Disk Usage"
+#sh mail.sh "DevOps Team" "High Disk Usage" "$message" "ravikumardarsi390@gmail.com" "ALERT High Disk Usage"
